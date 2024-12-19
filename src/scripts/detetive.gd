0eaacdad331c4db1ef0c17a9da8a1f674849234e
Bruno Sales <bruno.m.sales03@gmail.com>
2024-12-19 20:32:17 -0300

    added textboxes

--- src/scripts/detetive.gd
+++ src/scripts/detetive.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 @export_category("Variables")
 @export var _move_speed: float = 64.0
 @export var tamanho_inv: int = 8
@@ -23,10 +24,17 @@ func move() -> void:
 	
 	velocity = _direction.normalized() * _move_speed
 	
-	if _direction != Vector2.ZERO:
+	if _direction != Vector2.ZERO and canvas_layer.is_showing != true:
 		sprite_2d.rotation = _direction.angle()
 	
-	move_and_slide()
+		move_and_slide()
+		
+func show_dialogue(new_text) -> void:
+	canvas_layer.show_textbox(new_text)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_action_pressed("interact") :
+		canvas_layer.hide_textbox()
 	
 func kill() -> void:
 	Global.map.restart()
